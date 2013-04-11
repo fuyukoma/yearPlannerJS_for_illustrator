3acd379170eaca876188d9e9d560ac853076e457
小泉 ふゆか <fuyuka.k@gmail.com>
2013-04-11 21:47:06 +0900

    ドキュメント作成

---
+++ year_planner.js
@@ -0,0 +1,7 @@
+// Document Size
+// A4 : 210mm　* 297mm
+// point = mm * 2.835
+var doc = {"width": 210*2.835, "height": 297*2.835};
+
+documents.add(DocumentColorSpace.CMYK, doc.width, doc.height);
+
